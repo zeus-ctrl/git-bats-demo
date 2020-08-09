e1fc2aea27d2e299874f0980cb8e0a95e100c6c3
Nic McPhee <mcphee@morris.umn.edu>
2020-08-09 16:47:51 -0500

    Update solution to address `shellcheck` warnings
    This fixes a bunch of `shellcheck` warnings so the solution should be considered "clean" now.

--- count_successes.sh
+++ count_successes.sh
@@ -1,19 +1,19 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-SCRATCH=`mktemp --directory`
+SCRATCH=$(mktemp --directory)
 
-tgz_file=$1
+tgz_file="$1"
 
 # Extract everything from the tar file into the scratch directory.
-tar -zxf $tgz_file --directory $SCRATCH
+tar -zxf "$tgz_file" --directory "$SCRATCH"
 
 # Count the number of successes.
-num_successes=`grep -r SUCCESS $SCRATCH | wc -l`
+num_successes=$(grep -r SUCCESS "$SCRATCH" | wc -l)
 # Count the number of failures.
-num_failures=`grep -r FAILURE $SCRATCH | wc -l`
+num_failures=$(grep -r FAILURE "$SCRATCH" | wc -l)
 
 # Clean up the scratch directory
-rm -rf $SCRATCH
+rm -rf "$SCRATCH"
 
 # Print the result.
 echo "There were $num_successes successes and $num_failures failures."
